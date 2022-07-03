9ae749f6dfed44eabbe0cd3322e5d6a364bbb931
Alexandr Ivanov <arlert@mail.ru>
2022-07-03 18:49:05 +0300

    add test

--- src/Arda.Utilities/SampleTest/UnitTest1.cs
+++ src/Arda.Utilities/SampleTest/UnitTest1.cs
@@ -9,5 +9,11 @@ namespace SampleTest
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(false);
+        }
     }
 }
