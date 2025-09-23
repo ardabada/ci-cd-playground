db04a2e07450037faa68d9a5eb73a03525e2c0f8
Alexandr Ivanov <arlert@mail.ru>
2025-09-23 13:39:06 +0300

    changes

--- src/Arda.AnotherService/Arda.AnotherService/Controllers/WeatherForecastController.cs
+++ src/Arda.AnotherService/Arda.AnotherService/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace Arda.AnotherService.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //some feature changes
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
